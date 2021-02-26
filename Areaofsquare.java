d03548e2d5a8b06a0ed853fb3f70898256de4422
Swapnil Mishra <79589558+Swapnil250102@users.noreply.github.com>
2021-02-26 23:16:05 +0530

    Area of Square

--- Areaofsquare.java
+++ Areaofsquare.java
@@ -1,6 +1,7 @@
 public class Area of square{
   public static void main(String[] args){
     int side=5;
+    int area;
     area=side*side;
     System.out.println("Area="+area+"of a square with side"+side);
   }
