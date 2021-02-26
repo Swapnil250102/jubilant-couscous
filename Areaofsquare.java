5b17d29e84a807620fb07e82869d9162dbe6b05f
Swapnil Mishra <79589558+Swapnil250102@users.noreply.github.com>
2021-02-26 23:10:31 +0530

    Finding area of square

---
+++ Areaofsquare.java
@@ -0,0 +1,7 @@
+public class Area of square{
+  public static void main(String[] args){
+    int side=5;
+    area=side*side;
+    System.out.println("Area="+area+"of a square with side"+side);
+  }
+}
