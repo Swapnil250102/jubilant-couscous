da3ffd7b2fae6d3c4b00f135e41247f893fc9af6
Swapnil Mishra <79589558+Swapnil250102@users.noreply.github.com>
2021-02-26 23:15:23 +0530

    Perimeter of square

---
+++ Perimeterofsquare.java
@@ -0,0 +1,8 @@
+public class Perimeter of square{
+  public static void main(String[] args){
+    int side=5;
+    int perimeter;
+    perimeter=4*side;
+    System.out.println("Perimeter="+Perimeter+"of a square with side"+side);
+  }
+}
